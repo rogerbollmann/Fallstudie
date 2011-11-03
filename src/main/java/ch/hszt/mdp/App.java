1cf70904c416a9c8449c9a773364f43004534573
RAPHI <raphi@raphi-VirtualBox.(none)>
2011-11-03 19:16:52 +0100

    Hello world3 added in App.java

--- src/main/java/ch/hszt/mdp/App.java
+++ src/main/java/ch/hszt/mdp/App.java
@@ -5,5 +5,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 	}
 }
